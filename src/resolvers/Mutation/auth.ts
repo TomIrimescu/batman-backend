dfcccb1abd3d05c6abb055ec5ec6377263320a61
Tom Irimescu <tom@irimescu.com>
2023-01-15 18:00:33 -0500

    Add signup mut & refactor mut structure

---
+++ src/resolvers/Mutation/auth.ts
@@ -0,0 +1,24 @@
+import { Context } from "../../index";
+
+interface SignupArgs {
+  email: string;
+  name: string;
+  bio: string;
+  password: string;
+}
+
+export const authResolvers = {
+  signup: (
+    _: any,
+    { email, name, password, bio }: SignupArgs,
+    { prisma }: Context
+  ) => {
+    return prisma.user.create({
+      data: {
+        email,
+        name,
+        password,
+      },
+    });
+  },
+};
